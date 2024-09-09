8253f3dbcd7ec00c48f018100e741b91e743c7d9
jsb58p <jacobbiddinger@gmail.com>
2024-09-09 09:15:59 -0500

    Update Array_Modify_Imp.cpp

--- Array_Modify_Imp.cpp
+++ Array_Modify_Imp.cpp
@@ -1,6 +1,7 @@
 #include "Array_Modify.h"
 
 void Array_Modify::initializeArray() {
+	/*initialzing variables and opening file*/
 	int value = 0;
 	inputFile.open(fileName);
 	maxIndex = 0;
@@ -9,7 +10,7 @@ void Array_Modify::initializeArray() {
 	while (!inputFile.eof()) {
 		/*statement checks whether array bounds have been reached*/
 		if (maxIndex + 1 == capacity) {
-			int* temp = numbers;	//pointer to temporarily hold location of old array while elements are copied to new array
+			int* temp = numbers;		//pointer to temporarily hold location of old array while elements are copied to new array
 			capacity *= 2;			//increase capacity for creation of new array
 			numbers = new int[capacity];
 			for (int i = 0; i < maxIndex; i++) {	//placing values in new array
@@ -20,33 +21,33 @@ void Array_Modify::initializeArray() {
 		/*statement to detect spaces, which indicates the end of an integer*/
 		if (inputFile.peek() == ' ' || inputFile.peek() == '\n' || inputFile.peek() == '\r') {
 			numbers[maxIndex] = value;		//assigns the current integer from "value" to the element at maxIndex
-			value = 0;					//resets value to 0 in preparation for next element.
-			inputFile.ignore(2, ' ');   //ignore the space between current and next integer
+			value = 0;				//resets value to 0 in preparation for next element.
+			inputFile.ignore(2, ' ');   		//ignore the space between current and next integer
 			
-			maxIndex++;	//once the end of an integer has been reached, the maxIndex, counting the number of integers
+			maxIndex++;				//once the end of an integer has been reached, increment maxIndex. Counts the number of integers in the array
 
 		}
-		/*Following statement detects whether end of file has been reached*/
+		/*if there is no space, and end of file isn't reached, then the integer has multiple digits*/
 		else if(!inputFile.eof()) {
 			value *= 10;				//multiply value by 10, this allows for correct decimal placement for integers with multiple digits
-			value += inputFile.get() - 48;	//adds the next digit ("- 48" converts value from char to int)
+			value += inputFile.get() - 48;		//adds the next digit ("- 48" converts value from char to int)
 			
 			
 		}
 		/*This statement detects when the end of file is reached*/
 		else if (inputFile.eof()) {
-			numbers[maxIndex] = value;	//assigns "value" to the last index of the array when end of file is reached
+			numbers[maxIndex] = value;		//assigns "value" to the last element of the array when end of file is reached
 			
 		}
 	}
 }
 
 int Array_Modify::findNum(int integer, int position) const {
-    if (position<1 || position>maxIndex) {
-		throw std::out_of_range("In function findNum, the value of position is out of range.");
+    if (position<1 || position>maxIndex) {		//exception thrown if "position" is not possible in the array
+		throw std::out_of_range("In function findNum, the value of position is out of range."); 
 	}
 	int numPosition = 0;	//counts number of times the entered integer has been found
-	for (int i = 0; i <= maxIndex; i++) {	//iterates through array
+	for (int i = 0; i <= maxIndex; i++) {		//iterates through array
 		if (numbers[i] == integer) {		//detects when the value of the current array element is equal to the integer being searched
 			numPosition++;
 			if(numPosition == position){    //if numPosition has incremented to equal the 
@@ -59,39 +60,39 @@ int Array_Modify::findNum(int integer, int position) const {
 }
 
 int* Array_Modify::modifyNum(size_t index, int newValue) {
-	if (index<0 || index>maxIndex) {
+	if (index<0 || index>maxIndex) {	//exception thrown if "index" is not a valid array index
 		throw std::out_of_range("In function modifyNum, the value of index is out of range.");
 	}
-	numSwap[0] = newValue;	//first element contains new value
-	numSwap[1] = numbers[index];	//second element contains old value
-	numbers[index] = newValue; //assigns new value to the chosen array element
-	return numSwap;			//returns pointer to the array containing the new and old value
+	numSwap[0] = newValue;			//first element contains new value
+	numSwap[1] = numbers[index];		//second element contains old value
+	numbers[index] = newValue; 		//assigns new value to the chosen array element
+	return numSwap;				//returns pointer to the array containing the new and old value
 }
 
 
 void Array_Modify::addNum(int numAdded) {
 	/*following statement detects if array capacity has been reached*/
 	if (maxIndex + 1 == capacity) {
-		int* temp = numbers;	//pointer to temporarily hold location of old array while elements are copied to new array
+		int* temp = numbers;		//pointer to temporarily hold location of old array while elements are copied to new array
 		capacity *= 2;			//increase capacity for creation of new array
 		numbers = new int[capacity];
 		for (int i = 0; i < maxIndex; i++) {	//placing values in new array
 			numbers[i] = temp[i];
 		}
-		delete[] temp; //delete the old array
+		delete[] temp; 			//delete the old array
 	}
 	maxIndex++;			
-	numbers[maxIndex] = numAdded;	//assign numAdded to the last element of the array
+	numbers[maxIndex] = numAdded;		//assign numAdded to the last element of the array
 }
 
 
 void Array_Modify::removeNum(size_t index) {
-	if (index<0 || index>maxIndex) {
+	if (index<0 || index>maxIndex) {	//exception thrown if "index" is not a valid array index
 		throw std::out_of_range("In function removeNum, the value of index is out of range.");
 	}
 	while (index <= maxIndex) {
 		numbers[index] = numbers[index + 1];	//shifts array elements down one index, starting with the one being removed
 		index++;
 	}
-	maxIndex--;		//decrement maxIndex to account for 1 less element
+	maxIndex--;				//decrement maxIndex to account for 1 less element
 }
